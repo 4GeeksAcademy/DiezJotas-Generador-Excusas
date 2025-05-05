affd40908504388869417f44feb83af26eebc219
DiezJotas <jose.jdg37@gmail.com>
2025-05-05 08:43:17 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+let quien = ["El vagabundo", "Un champiñon","El tren","El elfo"];
+let accion = ["ha robado","ha atropellado","ha saltado","ha disparado"];
+let que = ["una vieja","mis deberes","a mi gato","mi ramen"];
+let cuando = ["al crepúsculo","en la final de parchís","cuando veia Naruto","mientras hacia el pino"];
+
+let aleatorioquien = Math.floor(Math.random() * quien.length);
+let aleatorioaccion = Math.floor(Math.random() * accion.length);
+let aleatorioquque = Math.floor(Math.random() * que.length);
+let aleatoriocuando = Math.floor(Math.random() * cuando.length);
+
+let excuse = `${quien[aleatorioquien]} ${accion[aleatorioaccion]} ${que[aleatorioquque]} ${cuando[aleatoriocuando]}`;
+
+let parrafo = document.getElementById("excuse");
+parrafo.innerText = excuse;
+}
